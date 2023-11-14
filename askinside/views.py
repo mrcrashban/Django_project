b3d2833be7ce56fab0017d4d37be72bc4fb3d587
mrcrashban <metelevkirill4@gmail.com>
2023-11-14 16:36:58 +0300

    done all templates with samples

--- askinside/views.py
+++ askinside/views.py
@@ -1,3 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpResponse
+from templates import layouts
+
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'question_page.html')
+
+
+def paginate(objects_list, request, per_page=10):
+    paginator = Paginator(objects_list, per_page)
+
+    page_number = request.GET.get('page', 1)
+
+    return paginator.get_page(page_number)
